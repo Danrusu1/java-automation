1fd53ff9544a08a97122f7d6954f18d33e56c058
McMary <mariacrisan6@gmail.com>
2020-10-27 17:02:11 +0200

    Updated pom and sample junit test

---
+++ maria-playground/src/test/java/sample/SampleJUnitTest.java
@@ -0,0 +1,12 @@
+package sample;
+
+/**
+ * Description: file created for java-automation
+ * Author: Dan Rusu (d.rusu@plan-net.com)
+ * Creation date: 26/10/2020
+ * Modified data:
+ * (c) 2020 Plan.Net-Technology - all copy, distribution not allowed without Plan.Net permission
+ */
+
+public class SampleJUnitTest {
+}
